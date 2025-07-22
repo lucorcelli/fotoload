205bd144e8fa8bb50e9785745a269b6180f7ee91
lucorcelli <lucorcelli@gmail.com>
2025-07-22 01:56:43 -0300

    Delete test-categoria.ts

--- test-categoria.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-async function main() {
-  const categorias = await prisma.categoria.findMany();
-  console.log(categorias);
-}
-
-main().finally(() => prisma.$disconnect());
